f1486cb3dae148065869cedba6a6870ffeea7f2e
Andrius Malakauskas <andrius@amdev.lt>
2022-04-23 15:42:26 +0300

    main code added

--- 000SerialCommunication/src/main.cpp
+++ 000SerialCommunication/src/main.cpp
@@ -1,9 +1,34 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <stdio.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <util/delay.h>
+#include <stdint.h>
+#include "usart_printf.h"
+
+#define F_CPU 16000000
+#define BLINK_DELAY_MS 100
+
+int main(void)
+{
+  usart_initialize();
+  stdout = &uart_output;
+
+
+  // Arduino digital pin 13 (pin 5 of PORTB) for output
+  DDRB |= 0B100000; // PORTB5
+  int x = 0;
+  while (1)
+  {
+    // turn LED on
+    PORTB |= 0B100000; // PORTB5
+    //printf("labas: %d", x);
+    x++;
+    _delay_ms(BLINK_DELAY_MS);
+
+    // turn LED off
+    PORTB &= ~0B100000; // PORTB5
+    _delay_ms(BLINK_DELAY_MS);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void _putchar(char character) {}
